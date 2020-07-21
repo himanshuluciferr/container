316e26236aa151dce4562fa7466dc5e7fe3a891b
himanshuluciferr <60671446+himanshuluciferr@users.noreply.github.com>
2020-07-21 09:38:07 +0530

    Add files via upload

---
+++ Controllers/WallpapersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WallpapeRAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WallpapersController : ControllerBase
+    {
+        public string[] getData()
+        {
+            return new string[] { "Welcome", "Good Morning" };
+        }
+    }
+}
